ed9980e01be3f551ca880a75d657daeed9b7429e
Felix Dietze <github@felx.me>
2024-08-12 19:52:02 +0200

    accept and return json arrays

--- service/server.jl
+++ service/server.jl
@@ -18,9 +18,9 @@ db = get_score_db(ENV["DATABASE_PATH"])
 route("/score", method = POST) do
     message = rawpayload()
     parsed_message = JSON.parse(message)
-    vote_events = map(parse_vote_event, parsed_message["payload"])
+    vote_events = map(parse_vote_event, parsed_message)
 
-    results = ""
+    results = "["
     n = 0
 
     map(vote_events) do vote_event
@@ -29,11 +29,15 @@ route("/score", method = POST) do
                 e = as_event(vote_event.vote_event_id, vote_event.vote_event_time, score_or_effect)
                 insert_event(db, e)
                 json_data = JSON.json(e)
+                if n > 0
+                  results *= ","
+                end
                 results *= json_data * "\n"
                 n += 1
             end
         process_vote_event(emit_event, db, vote_event)
     end
+    results *= "]"
 
     return results
 end
